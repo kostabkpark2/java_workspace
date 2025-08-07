77dd5da233717351776201b2458f153b1d4d64aa
kate <kostabkpark2@gmail.com>
2025-08-07 10:26:52 +0900

    ch07 ex_3_3

---
+++ src/ch07/exercise/EX_3_3.java
@@ -0,0 +1,47 @@
+package ch07.exercise;
+
+import java.util.Arrays;
+
+public class EX_3_3 {
+
+	public static void main(String[] args) {
+		Book[] books = { new Book(15000), new Book(50000), new Book(20000) };
+
+		System.out.println("정렬 전");
+		for (Book book : books) {
+			System.out.println(book);
+		}
+
+		Arrays.sort(books);
+
+		System.out.println("정렬 전");
+		for (Book book : books) {
+			System.out.println(book);
+		}
+
+	}
+
+}
+
+class Book implements Comparable {
+	int price;
+
+	public Book(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (o instanceof Book b) {
+			return this.price - b.price;
+		}
+		return -9999999;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return String.format("Book [price = %d]", price);
+	}
+
+}
