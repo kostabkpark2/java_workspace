ed148e6a6bee0e449e2cca80d1a2d0a8b5b318ea
kate <kostabkpark2@gmail.com>
2025-08-08 10:47:01 +0900

    EX_2

---
+++ src/ch08/RandomDemo.java
@@ -0,0 +1,16 @@
+package ch08;
+
+import java.util.Random;
+
+public class RandomDemo {
+
+	public static void main(String[] args) {
+		Random r = new Random();
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(r.nextLong(1000000001L));
+		}
+
+	}
+
+}
