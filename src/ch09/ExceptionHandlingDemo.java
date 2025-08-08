469f1788483b406bba109c237cf57411eee1ad85
kate <kostabkpark2@gmail.com>
2025-08-08 12:53:02 +0900

    ch09 exception handling - update

--- src/ch09/ExceptionHandlingDemo.java
+++ src/ch09/ExceptionHandlingDemo.java
@@ -3,8 +3,25 @@ package ch09;
 public class ExceptionHandlingDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			method1();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			System.out.println("문자를 포함한 경우에는 숫자로 변환할 수 없습니다.");
+		}
 	}
 
+	static void method1() throws InterruptedException, NumberFormatException {
+		method2();
+	}
+
+	static void method2() throws InterruptedException, NumberFormatException {
+
+		Thread.sleep(100);
+		int n = Integer.parseInt("abc");
+
+	}
 }
