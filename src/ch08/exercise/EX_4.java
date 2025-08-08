2df71a941c7e2ac1dfe20485cbfaa99bae00e79f
kate <kostabkpark2@gmail.com>
2025-08-08 11:51:01 +0900

    ch09 unchecked exception

---
+++ src/ch08/exercise/EX_4.java
@@ -0,0 +1,23 @@
+package ch08.exercise;
+
+import java.util.Random;
+
+public class EX_4 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println(new Dice().roll());
+		}
+
+	}
+
+}
+
+class Dice {
+	int roll() {
+		Random r = new Random();
+
+		return r.nextInt(1, 7);
+	}
+}
---
+++ src/ch09/UnCheckedDemo.java
@@ -0,0 +1,28 @@
+package ch09;
+
+import java.util.StringTokenizer;
+
+public class UnCheckedDemo {
+
+	public static void main(String[] args) {
+		// int res = 5 / 0; // ArithmeticException
+
+		String str = "";
+
+		System.out.println(str.length()); // NullPointerException
+
+		int[] arr = new int[0];
+
+		System.out.println(arr.length); // NullPointerException
+
+		// System.out.println(arr[3]); // ArrayIndexOutOfBoundsException
+
+		String s = "a b c";
+		StringTokenizer st = new StringTokenizer(s);
+		while (st.hasMoreTokens()) {
+			st.nextToken();
+		}
+		st.nextToken();
+	}
+
+}
