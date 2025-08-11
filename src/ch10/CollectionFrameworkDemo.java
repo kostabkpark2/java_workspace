0ec956189a2a82c39f500704431ab9d5a7e1f3c6
kate <kostabkpark2@gmail.com>
2025-08-11 16:45:17 +0900

    Functional interface and Collection Framework - 2

--- src/ch10/CollectionFrameworkDemo.java
+++ src/ch10/CollectionFrameworkDemo.java
@@ -12,8 +12,55 @@ public class CollectionFrameworkDemo {
 		list.add("cherry");
 
 		list.forEach((s) -> System.out.println(s)); // Consumer 인터페이스의 구현체를 람다식으로 작성
-		list.removeIf((s) -> s.length() == 0);
+		list.removeIf((s) -> s.length() == 0); // Predicate 인터페이스의 구현체를 람다식으로 작성
 		list.forEach((s) -> System.out.println(s)); // Consumer 인터페이스의 구현체를 람다식으로 작성
+
+		// 숫자를 원소로 가지는 ArrayList 를 작성해라. 그리고 원소를 4개 추가하는데 + : 2개 -: 2 개
+		// 리스트에서 음수값은 삭제하고 양수값만 남겨놓은 다음, 남아있는 원소를 전부 출력해주세요.
+		ArrayList<Integer> numberList = new ArrayList<>();
+		numberList.add(-5);
+		numberList.add(-3);
+		numberList.add(15);
+		numberList.add(7);
+
+		numberList.forEach((n) -> System.out.println(n));
+		numberList.removeIf((n) -> n < 0); // Predicate 인터페이스의 구현체를 람다식으로 작성
+		numberList.forEach((n) -> System.out.println(n));
+
+		// 객체(삼각형)을 원소로 가지는 ArrayList 를 작성해라. 원소를 3개 추가하는데 ( ) 밑변과 높이가 (10,3), (10,5),
+		// (5,5) 인 객체이다.
+		// 넓이가 20 보다 작은 것은 리스트에서 삭제하고 남아있는 원소를 전부 출력해주세요.
+
+		ArrayList<Triangle> triangleList = new ArrayList<>();
+		triangleList.add(new Triangle(10, 3));
+		triangleList.add(new Triangle(10, 5));
+		triangleList.add(new Triangle(5, 5));
+
+		System.out.println("삼각형 리스트 출력");
+		triangleList.forEach(t -> System.out.println(t));
+		triangleList.removeIf(t -> t.findArea() < 20);
+		System.out.println("넓이가 20 보다 큰 삼각형 리스트 출력");
+		triangleList.forEach(t -> System.out.println(t));
 	}
 
+}
+
+class Triangle {
+	int height;
+	int width;
+
+	public Triangle(int width, int height) {
+		this.width = width;
+		this.height = height;
+	}
+
+	double findArea() {
+		return (width * height) / (double) 2;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Triangle[area = " + findArea() + "]";
+	}
 }
