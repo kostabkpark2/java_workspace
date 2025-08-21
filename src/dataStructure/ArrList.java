44a8a873d07c0c093a1f99b24a10b752a3ee3a6e
kate <kostabkpark2@gmail.com>
2025-08-21 11:22:16 +0900

    ArrList-remove(index), add(index, element) - 2

--- src/dataStructure/ArrList.java
+++ src/dataStructure/ArrList.java
@@ -13,15 +13,7 @@ public class ArrList<E> {
 	public boolean add(E e) { // 배열의 맨 마지막에 추가하는 연산
 		if (length() == size()) {
 			// 배열의 사이즈를 2배 증가시킨다. <==== (1. 여기 구현해보기)
-			System.out.println("before ==> 내부 부품 배열의 크기 " + length());
-			E[] t = (E[]) (new Object[length() * 2]);
-
-			for (int i = 0; i < size(); i++) {
-				t[i] = a[i];
-			}
-			// t = Arrays.copyOf(a, size);
-			a = t;
-			System.out.println("after ==> 내부 부품 배열의 크기 " + length());
+			resize(length() * 2);
 		}
 		// 맨 마지막에 추가하고 배열의 size 를 증가시킨다. <==== (1. 여기 구현해보기)
 		a[size] = e;
@@ -32,15 +24,7 @@ public class ArrList<E> {
 	public boolean add(int index, E e) { // 배열의 중간, index 의 위치에 항목을 추가하는 연산
 		if (length() == size()) {
 			// 배열의 사이즈를 2배 증가시킨다. <==== (1. 여기 구현해보기)
-			System.out.println("before ==> 내부 부품 배열의 크기 " + length());
-			E[] t = (E[]) (new Object[length() * 2]);
-
-			for (int i = 0; i < size(); i++) {
-				t[i] = a[i];
-			}
-//			t = Arrays.copyOf(a, size);
-			a = t;
-			System.out.println("after ==> 내부 부품 배열의 크기 " + length());
+			resize(length() * 2);
 		}
 		// index 의 위치에 항목을 추가하기 위해 index 부터 size - 1 까지의 항목들을 한칸씩 이동시킨다.
 		// 이 때 맨 마지막 항목부터 이동시켜야 함 주의 !!!
@@ -70,18 +54,7 @@ public class ArrList<E> {
 
 		if (size > 0 && size <= length() / 4) {
 			// 배열의 크기를 반으로 줄인다.
-			System.out.println("before ==> 내부 부품 배열의 크기 " + length());
-			E[] t = (E[]) (new Object[length() / 2]);
-
-			// 기존배열의 값을 새로 만든 배열로 옮긴다.
-			for (int i = 0; i < size(); i++) {
-				t[i] = a[i];
-			}
-//			t = Arrays.copyOf(a, size - 1);
-			// 기존배열을 없애고 새로만든 배열을 a 가 참조한다. <==== (3. 여기 구현해보기, 코드가 길어지고 재사용되면 나중에 리팩토링 하기)
-			a = t;
-			System.out.println("after ==> 내부 부품 배열의 크기 " + length());
-
+			resize(length() / 2);
 		}
 		return removed; // 백업 받아놓은 삭제된 요소를 반환한다. <==== (3. 여기 구현해보기)
 	}
@@ -103,33 +76,27 @@ public class ArrList<E> {
 
 		if (size > 0 && size <= length() / 4) {
 			// 배열의 크기를 반으로 줄인다.
-			System.out.println("before ==> 내부 부품 배열의 크기 " + length());
-			E[] t = (E[]) (new Object[length() / 2]);
-
-			// 기존배열의 값을 새로 만든 배열로 옮긴다.
-			for (int i = 0; i < size(); i++) {
-				t[i] = a[i];
-			}
-//			t = Arrays.copyOf(a, size - 1);
-			// 기존배열을 없애고 새로만든 배열을 a 가 참조한다. <==== (3. 여기 구현해보기, 코드가 길어지고 재사용되면 나중에 리팩토링 하기)
-			a = t;
-			System.out.println("after ==> 내부 부품 배열의 크기 " + length());
-
+			resize(length() / 2);
 		}
 		return removed; // 백업 받아놓은 삭제된 요소를 반환한다. <==== (3. 여기 구현해보기)
 	}
 
 	private void resize(int length) {
-		// 배열의 크기를 length 만큼 증가 혹은 감소시킨다.
+		// 배열의 크기를 length 만큼 증가 혹은 감소시킨다.// <==== (4. 여기 구현해보기)
+		System.out.println("before ==> 내부 부품 배열의 크기 " + length());
+		E[] t = (E[]) (new Object[length]);
+
 		// 기존배열의 값을 새로 만든 배열로 옮긴다.
+		for (int i = 0; i < size(); i++) {
+			t[i] = a[i];
+		}
 		// 기존배열을 없애고 새로만든 배열을 a 가 참조한다.
-
-		// <==== (4. 여기 구현해보기)
-
+		a = t;
+		System.out.println("after ==> 내부 부품 배열의 크기 " + length());
 		// 자바의 ArrayList 는 Arrays.copyOf , arraycopy 등과 같은 방법으로 최적화되어 있다.
-		// 우리는 반복문으로 옮겨보자.
-
-		// <==== (4. 여기 구현해보기)
+		// 우리는 반복문으로 옮겨보자
+//		t = Arrays.copyOf(a, size);
+		// 기존배열을 없애고 새로만든 배열을 a 가 참조한다. <==== (3. 여기 구현해보기, 코드가 길어지고 재사용되면 나중에 리팩토링 하기)
 	}
 
 	// 기타 편의 메서드 구현 //<==== (5. 여기 3개의 메서드 구현해보기)
